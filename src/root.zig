c529267c39c398f3cc56e582e241e1eda8c4ef76
PierreLgol <plgol.perso@gmail.com>
2025-10-05 12:59:41 +0200

    improvements

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,6 @@
 const std = @import("std");
 pub const zdt = @import("zdt");
 pub const log = @import("log/log.zig");
+pub const Logger = log.Logger;
+pub const LogTime = log.LogTime;
+pub const LogLevel = log.LogLevel;
